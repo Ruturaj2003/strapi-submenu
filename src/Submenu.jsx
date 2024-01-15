072e8aea9f7ad8d9452fdc54801f558555a03211
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-01-15 19:40:52 +0530

    Update Styles

--- src/Submenu.jsx
+++ src/Submenu.jsx
@@ -1,4 +1,49 @@
+import { useGlobalContext } from './Context';
+import sublinks from './data';
+import { useRef } from 'react';
 const Submenu = () => {
-  return <h2>Submenu</h2>;
+  const { pageId, setPageId } = useGlobalContext();
+  const currentPage = sublinks.find((item) => {
+    if (item.pageId === pageId) {
+      return item;
+    }
+    return false;
+  });
+
+  const handleMouseLeave = (e) => {
+    const submenu = submenuContainer.current;
+    const { left, right, bottom } = submenu.getBoundingClientRect();
+    const { clientX, clientY } = e;
+    if (clientX < left - 1 || clientX > right - 1 || clientY > bottom) {
+      setPageId(null);
+    }
+  };
+
+  const submenuContainer = useRef(null);
+
+  return (
+    <div className={currentPage ? 'submenu show-submenu' : 'submenu'}>
+      <h5>{currentPage?.page}</h5>
+      <div
+        onMouseLeave={handleMouseLeave}
+        className="submenu-links"
+        style={{
+          gridTemplateColumns:
+            currentPage?.links?.length > 3 ? '1fr 1fr' : '1fr',
+        }}
+        ref={submenuContainer}
+      >
+        {currentPage?.links?.map((link) => {
+          const { id, url, label, icon } = link;
+          return (
+            <a key={id} href={url}>
+              {icon}
+              {label}
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 export default Submenu;
